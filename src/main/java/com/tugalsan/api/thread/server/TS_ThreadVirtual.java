784a5ed7cd70f8e9bb447addf75a4f4d5b56be12
tugalsan karabacak <tugalsan@gmail.com>
2022-08-05 13:07:35 +0300

    autogenerate pom tryout

---
+++ src/main/java/com/tugalsan/api/thread/server/TS_ThreadVirtual.java
@@ -0,0 +1,15 @@
+package com.tugalsan.api.thread.server;
+
+public class TS_ThreadVirtual {
+
+//    public static void run() {
+//        try ( var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+//            IntStream.range(0, 10_000).forEach(i -> {
+//                executor.submit(() -> {
+//                    Thread.sleep(Duration.ofSeconds(1));
+//                    return i;
+//                });
+//            });
+//        }
+//    }
+}
