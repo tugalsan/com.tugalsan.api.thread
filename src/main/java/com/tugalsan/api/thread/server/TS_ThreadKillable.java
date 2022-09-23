082537a855bbe273ff912705a7fe852cbd8655be
tugalsan karabacak <tugalsan@gmail.com>
2022-09-23 09:05:05 +0300

    Update TS_ThreadKillable.java

--- src/main/java/com/tugalsan/api/thread/server/TS_ThreadKillable.java
+++ src/main/java/com/tugalsan/api/thread/server/TS_ThreadKillable.java
@@ -1,16 +1,17 @@
 package com.tugalsan.api.thread.server;
 
 import com.tugalsan.api.executable.client.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 abstract public class TS_ThreadKillable implements TGS_Executable, TS_ThreadKillableInterface {
 
     public boolean isKillMe() {
-        return killMe;
+        return killMe.get();
     }
 
     public void setKillMe(boolean killMe) {
-        this.killMe = killMe;
+        this.killMe.set(killMe);
     }
 
-    private boolean killMe = false;
+    private AtomicBoolean killMe = new AtomicBoolean(false);
 }
