7b0339128550dab84425b22b03ca665b0b9505f3
tugalsan karabacak <tugalsan@gmail.com>
2022-09-29 12:51:02 +0300

    preview compile

--- src/main/java/com/tugalsan/api/thread/server/TS_ThreadRunUtils.java
+++ src/main/java/com/tugalsan/api/thread/server/TS_ThreadRunUtils.java
@@ -8,14 +8,14 @@ public class TS_ThreadRunUtils {
 
     //TODO ENABLE VIRTUAL THREAD
     public static void now(TGS_Executable exe) {
-//        Thread. startVirtualThread(() -> exe.execute());
-        new Thread(() -> exe.execute()).start();
+        Thread.startVirtualThread(() -> exe.execute());
+//        new Thread(() -> exe.execute()).start();
     }
 
-    @Deprecated //AFTER VIRTUAL THREAD INTEGRATION, NO NEED FOR LAZY EXECUTION, JUST USE now
-    public static void once(TGS_Executable exe) {
-        TS_ThreadPoolUtils.execute(exe);
-    }
+//    @Deprecated //AFTER VIRTUAL THREAD INTEGRATION, NO NEED FOR LAZY EXECUTION, JUST USE now
+//    public static void once(TGS_Executable exe) {
+//        TS_ThreadPoolUtils.execute(exe);
+//    }
 
     public static void everySeconds(boolean startNow, long initialDelayAndPeriod, TGS_Executable exe) {
         TS_ThreadPoolUtils.scheduleAtFixedRate(exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.SECONDS);
--- src/main/java/com/tugalsan/api/thread/server/pool/TS_ThreadPoolUtils.java
+++ src/main/java/com/tugalsan/api/thread/server/pool/TS_ThreadPoolUtils.java
@@ -6,31 +6,31 @@ import java.util.concurrent.*;
 
 public class TS_ThreadPoolUtils {
 
-    @Deprecated //JUST USE VIRTUAL THREAD
-    final private static TGS_ListSyncItem<ExecutorService> IMMEDIATE = new TGS_ListSyncItem(Executors.newSingleThreadExecutor());
+//    @Deprecated //JUST USE VIRTUAL THREAD
+//    final private static TGS_ListSyncItem<ExecutorService> IMMEDIATE = new TGS_ListSyncItem(Executors.newSingleThreadExecutor());
 
     //TODO ENABLE VIRTUAL THREAD
-    //final private static TGS_ListSyncItem<ScheduledExecutorService> SCHEDULED = new TGS_ListSyncItem(Executors.newSingleThreadScheduledExecutor(Thread.ofVirtual().factory()));
-    final private static TGS_ListSyncItem<ScheduledExecutorService> SCHEDULED = new TGS_ListSyncItem(Executors.newScheduledThreadPool(1));
+    final private static TGS_ListSyncItem<ScheduledExecutorService> SCHEDULED = new TGS_ListSyncItem(Executors.newSingleThreadScheduledExecutor(Thread.ofVirtual().factory()));
+//    final private static TGS_ListSyncItem<ScheduledExecutorService> SCHEDULED = new TGS_ListSyncItem(Executors.newScheduledThreadPool(1));
 
     public static void shutdown() {
-        IMMEDIATE.get().shutdown();
+//        IMMEDIATE.get().shutdown();
         SCHEDULED.get().shutdown();
     }
 
-    private static void execute(Runnable exe) {
-        IMMEDIATE.get().execute(exe);
-    }
+//    private static void execute(Runnable exe) {
+//        IMMEDIATE.get().execute(exe);
+//    }
 
     private static void scheduleAtFixedRate(Runnable r, long initialDelay, long period, TimeUnit unit) {
         SCHEDULED.get().scheduleAtFixedRate(r, initialDelay, period, unit);
     }
 
-    @Deprecated //JUST USE VIRTUAL THREAD
-    public static void execute(TGS_Executable exe) {
-        Runnable r = () -> exe.execute();
-        execute(r);
-    }
+//    @Deprecated //JUST USE VIRTUAL THREAD
+//    public static void execute(TGS_Executable exe) {
+//        Runnable r = () -> exe.execute();
+//        execute(r);
+//    }
 
     public static void scheduleAtFixedRate(TGS_Executable exe, long initialDelay, long period, TimeUnit unit) {
         Runnable r = () -> exe.execute();
