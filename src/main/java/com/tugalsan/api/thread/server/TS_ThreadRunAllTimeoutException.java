2183439b80f7d3ca1f69481831f01eda602e3272
tugalsan karabacak <tugalsan@gmail.com>
2022-10-10 08:55:30 +0300

    minor

--- src/main/java/com/tugalsan/api/thread/server/TS_ThreadRunAllTimeoutException.java
+++ src/main/java/com/tugalsan/api/thread/server/TS_ThreadRunAllTimeoutException.java
@@ -2,4 +2,7 @@ package com.tugalsan.api.thread.server;
 
 public class TS_ThreadRunAllTimeoutException extends RuntimeException {
 
+    public TS_ThreadRunAllTimeoutException() {
+        super("Timeout Error Occured While Running TS_ThreadRunAllX");
+    }
 }
