ba9cfb50966783d2f0bb24c87764dad04c87ee5a
tugalsan karabacak <tugalsan@gmail.com>
2023-07-31 09:12:39 +0300

    .

--- src/main/java/com/tugalsan/api/thread/server/killable/TS_ThreadKillableBuilder.java
+++ src/main/java/com/tugalsan/api/thread/server/killable/TS_ThreadKillableBuilder.java
@@ -2,6 +2,10 @@ package com.tugalsan.api.thread.server.killable;
 
 public class TS_ThreadKillableBuilder {
 
+    public static TS_ThreadKillableBuilder1Class of() {
+        return ofClass(Object.class);
+    }
+
     public static <T> TS_ThreadKillableBuilder1Class<T> ofClass(Class<T> clazz) {
         return new TS_ThreadKillableBuilder1Class(clazz);
     }
