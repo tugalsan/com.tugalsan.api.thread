c667cce6d31b4d72d9268102a55325c2296cd02b
tugalsan karabacak <tugalsan@gmail.com>
2023-07-31 10:30:04 +0300

    .

--- src/main/java/com/tugalsan/api/thread/server/async/TS_ThreadAsync.java
+++ src/main/java/com/tugalsan/api/thread/server/async/TS_ThreadAsync.java
@@ -1,40 +1,9 @@
 package com.tugalsan.api.thread.server.async;
 
-import com.tugalsan.api.callable.client.TGS_Callable;
 import com.tugalsan.api.runnable.client.*;
-import com.tugalsan.api.unsafe.client.TGS_UnSafe;
-import com.tugalsan.api.validator.client.TGS_Validator;
 import java.util.concurrent.*;
 
 public class TS_ThreadAsync {
-
-//    public static <T> Thread nowWhile(TGS_Validator prePeriodicValidate, TGS_RunnableType1<T> periodicRun) {
-//        return nowWhile(null, prePeriodicValidate, periodicRun, null);
-//    }
-//
-//    public static <T> Thread nowWhile(TGS_Callable<T> init, TGS_Validator prePeriodicValidate, TGS_RunnableType1<T> periodicRun) {
-//        return nowWhile(init, periodicRun, periodicRun, null);
-//    }
-//
-//    public static <T> Thread nowWhile(TGS_Validator prePeriodicValidate, TGS_RunnableType1<T> periodicRun, TGS_RunnableType1<T> finalize) {
-//        return nowWhile(null, periodicRun, periodicRun, finalize);
-//    }
-
-//    public static <T> Thread nowWhile(TGS_Callable<T> init, TGS_Validator prePeriodicValidate, TGS_RunnableType1<T> periodicRun, TGS_RunnableType1<T> finalize) {
-//        //TODO
-//        now(() -> {
-//            var o = init.call();
-//            while (prePeriodicValidate.validate()) {
-//                var begin = System.currentTimeMillis();
-//                images.add(TS_InputScreenUtils.shotPictures(r, rect));
-//                var end = System.currentTimeMillis();
-//                TGS_UnSafe.run(() -> Thread.sleep(gif.timeBetweenFramesMS - (end - begin)));
-//                Thread.yield();
-//            }
-//            finalize.run(o);
-//            captureAlive.set(false);
-//        });
-//    }
 
     public static Thread now(TGS_Runnable exe) {
         return Thread.startVirtualThread(() -> exe.run());
