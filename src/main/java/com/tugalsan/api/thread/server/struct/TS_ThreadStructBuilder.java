dc20c0eb7493e88db7b605c849d176ffde937462
tugalsan karabacak <tugalsan@gmail.com>
2023-08-01 08:17:02 +0300

    .

--- src/main/java/com/tugalsan/api/thread/server/struct/TS_ThreadStructBuilder.java
+++ src/main/java/com/tugalsan/api/thread/server/struct/TS_ThreadStructBuilder.java
@@ -1,6 +1,7 @@
 package com.tugalsan.api.thread.server.struct;
 
 import com.tugalsan.api.callable.client.TGS_Callable;
+import com.tugalsan.api.runnable.client.TGS_RunnableType1;
 import com.tugalsan.api.runnable.client.TGS_RunnableType2;
 import java.time.Duration;
 import java.util.Optional;
@@ -24,19 +25,23 @@ public class TS_ThreadStructBuilder {
         return new TS_ThreadStructBuilder1Init("Unnamed", TS_ThreadStructCallableTimed.of(max, call));
     }
 
-    public static <T> TS_ThreadStructBuilder2Main<T> main(TGS_RunnableType2<AtomicBoolean, T> killTriggered_initObj) {
+    public static <T> TS_ThreadStructBuilder2Main<T> main(TGS_RunnableType1<AtomicBoolean> killTriggered) {
+        TGS_RunnableType2<AtomicBoolean, Object> killTriggered_initObj = (kt, initObj) -> killTriggered.run(kt);
         return new TS_ThreadStructBuilder2Main("Unnamed", TS_ThreadStructCallableTimed.of(), TS_ThreadStructRunnableTimedType2.run(killTriggered_initObj));
     }
 
-    public static <T> TS_ThreadStructBuilder2Main<T> mainTimed(Duration max, TGS_RunnableType2<AtomicBoolean, T> killTriggered_initObj) {
+    public static <T> TS_ThreadStructBuilder2Main<T> mainTimed(Duration max, TGS_RunnableType1<AtomicBoolean> killTriggered) {
+        TGS_RunnableType2<AtomicBoolean, Object> killTriggered_initObj = (kt, initObj) -> killTriggered.run(kt);
         return new TS_ThreadStructBuilder2Main("Unnamed", TS_ThreadStructCallableTimed.of(), TS_ThreadStructRunnableTimedType2.maxTimedRun(max, killTriggered_initObj));
     }
 
-    public static <T> TS_ThreadStructBuilder2Main<T> asyncRun(TGS_RunnableType2<AtomicBoolean, T> killTriggered_initObj) {
+    public static <T> TS_ThreadStructBuilder2Main<T> asyncRun(TGS_RunnableType1<AtomicBoolean> killTriggered) {
+        TGS_RunnableType2<AtomicBoolean, Object> killTriggered_initObj = (kt, initObj) -> killTriggered.run(kt);
         return new TS_ThreadStructBuilder2Main("Unnamed", TS_ThreadStructCallableTimed.of(), TS_ThreadStructRunnableTimedType2.run(killTriggered_initObj));
     }
 
-    public static <T> TS_ThreadStruct<T> asyncRun(Duration max, TGS_RunnableType2<AtomicBoolean, T> killTriggered_initObj) {
+    public static <T> TS_ThreadStruct<T> asyncRun(Duration max, TGS_RunnableType1<AtomicBoolean> killTriggered) {
+        TGS_RunnableType2<AtomicBoolean, Object> killTriggered_initObj = (kt, initObj) -> killTriggered.run(kt);
         var main = new TS_ThreadStructBuilder2Main("Unnamed", TS_ThreadStructCallableTimed.of(), TS_ThreadStructRunnableTimedType2.maxTimedRun(max, killTriggered_initObj));
         return TS_ThreadStruct.of(main.name, main.init, main.main, TS_ThreadStructRunnableTimedType1.empty(), Optional.empty(), Optional.empty());
     }
