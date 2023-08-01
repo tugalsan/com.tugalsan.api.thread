37396993be84e31cd6f14321370c07b306868175
tugalsan karabacak <tugalsan@gmail.com>
2023-08-01 08:05:41 +0300

    .

--- src/main/java/com/tugalsan/api/thread/server/struct/TS_ThreadStructBuilder.java
+++ src/main/java/com/tugalsan/api/thread/server/struct/TS_ThreadStructBuilder.java
@@ -11,23 +11,23 @@ public class TS_ThreadStructBuilder {
         return new TS_ThreadStructBuilder0Name(name);
     }
     
-    public <T> TS_ThreadStructBuilder1Init<T> initEmpty() {
+    public static <T> TS_ThreadStructBuilder1Init<T> initEmpty() {
         return new TS_ThreadStructBuilder1Init("Unnamed", TS_ThreadStructCallableTimed.of());
     }
 
-    public <T> TS_ThreadStructBuilder1Init<T> init(TGS_Callable<T> call) {
+    public static <T> TS_ThreadStructBuilder1Init<T> init(TGS_Callable<T> call) {
         return new TS_ThreadStructBuilder1Init("Unnamed", TS_ThreadStructCallableTimed.of(call));
     }
 
-    public <T> TS_ThreadStructBuilder1Init<T> initTimed(Duration max, TGS_Callable<T> call) {
+    public static <T> TS_ThreadStructBuilder1Init<T> initTimed(Duration max, TGS_Callable<T> call) {
         return new TS_ThreadStructBuilder1Init("Unnamed", TS_ThreadStructCallableTimed.of(max, call));
     }
 
-    public <T> TS_ThreadStructBuilder2Main<T> main(TGS_RunnableType2<AtomicBoolean, T> run) {
+    public static <T> TS_ThreadStructBuilder2Main<T> main(TGS_RunnableType2<AtomicBoolean, T> run) {
         return new TS_ThreadStructBuilder2Main("Unnamed", TS_ThreadStructCallableTimed.of(), TS_ThreadStructRunnableTimedType2.run(run));
     }
 
-    public <T> TS_ThreadStructBuilder2Main<T> mainTimed(Duration max, TGS_RunnableType2<AtomicBoolean, T> run) {
+    public static <T> TS_ThreadStructBuilder2Main<T> mainTimed(Duration max, TGS_RunnableType2<AtomicBoolean, T> run) {
         return new TS_ThreadStructBuilder2Main("Unnamed", TS_ThreadStructCallableTimed.of(), TS_ThreadStructRunnableTimedType2.maxTimedRun(max, run));
     }
 }
