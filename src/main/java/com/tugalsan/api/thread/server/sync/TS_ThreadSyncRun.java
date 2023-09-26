d9e065c9982dfc2518c6472e5e6a924d375d735d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-26 21:44:16 +0300

    .

--- src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncRun.java
+++ src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncRun.java
@@ -1,7 +1,6 @@
 package com.tugalsan.api.thread.server.sync;
 
 import com.tugalsan.api.stream.client.TGS_StreamUtils;
-import com.tugalsan.api.runnable.client.TGS_Runnable;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -9,13 +8,13 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class TS_ThreadSyncRun {
 
-    private TS_ThreadSyncRun(TGS_Runnable run) {
+    private TS_ThreadSyncRun(Runnable run) {
         this.run = run;
     }
-    final public ReentrantLock lock = new ReentrantLock();
-    final public TGS_Runnable run;
+    final private ReentrantLock lock = new ReentrantLock();
+    final private Runnable run;
 
-    public static TS_ThreadSyncRun of(TGS_Runnable run) {
+    public static TS_ThreadSyncRun of(Runnable run) {
         return new TS_ThreadSyncRun(run);
     }
 
@@ -24,10 +23,7 @@ public class TS_ThreadSyncRun {
             if (!lock.tryLock(timeout.toSeconds(), TimeUnit.SECONDS)) {
                 return;
             }
-            TGS_UnSafe.run(() -> run.run(), ex -> {
-                lock.unlock();
-                TGS_UnSafe.thrw(ex);
-            }, () -> lock.unlock());
+            TGS_UnSafe.run(() -> run.run(), ex -> TGS_UnSafe.thrw(ex), () -> lock.unlock());
         }, e -> TGS_StreamUtils.runNothing());
     }
 }
