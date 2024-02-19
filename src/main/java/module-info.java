81b56b802cbeced7963634358b0fc536bbb86d87
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-19 15:41:42 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,8 @@ module com.tugalsan.api.thread {
     exports com.tugalsan.api.thread.client;
     exports com.tugalsan.api.thread.server;
     exports com.tugalsan.api.thread.server.async;
+    exports com.tugalsan.api.thread.server.async.rateLimited;
+    exports com.tugalsan.api.thread.server.sync.lockLimited;
     exports com.tugalsan.api.thread.server.async.core;
     exports com.tugalsan.api.thread.server.async.builder;
     exports com.tugalsan.api.thread.server.sync;
