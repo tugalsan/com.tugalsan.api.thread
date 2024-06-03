ede19943da369e37a6f698036820663642386739
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-03 10:23:18 +0300

    Update TS_ThreadAsyncScheduled.java

--- src/main/java/com/tugalsan/api/thread/server/async/TS_ThreadAsyncScheduled.java
+++ src/main/java/com/tugalsan/api/thread/server/async/TS_ThreadAsyncScheduled.java
@@ -1,5 +1,6 @@
 package com.tugalsan.api.thread.server.async;
 
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.runnable.client.TGS_RunnableType1;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import java.time.Duration;
@@ -10,46 +11,53 @@ import java.util.concurrent.TimeUnit;
 //USE TS_ThreadAsyncBuilder with killTrigger if possible
 public class TS_ThreadAsyncScheduled {
 
+    final private static TS_Log d = TS_Log.of(false, TS_ThreadAsyncScheduled.class);
+
     final private static ScheduledExecutorService SCHEDULED = Executors.newSingleThreadScheduledExecutor(Thread.ofVirtual().factory());
 
     public static void destroy() {
         SCHEDULED.shutdown();
     }
 
-    private static void _scheduleAtFixedRate(TS_ThreadSyncTrigger killTrigger, Runnable exe, long initialDelay, long period, TimeUnit unit) {
+    private static boolean _scheduleAtFixedRate(TS_ThreadSyncTrigger killTrigger, Runnable exe, long initialDelay, long period, TimeUnit unit) {
+        if (period <= 0) {
+            d.ce("_scheduleAtFixedRate", "ERROR: period <= 0", "period", period);
+            return false;
+        }
         var future = SCHEDULED.scheduleAtFixedRate(exe, initialDelay, period, unit);
         TS_ThreadAsyncBuilder.of(killTrigger).mainDummyForCycle()
                 .fin(() -> future.cancel(false))
                 .cycle_mainValidation_mainPeriod(o -> !future.isCancelled() && !future.isDone(), Duration.ofMinutes(1))
                 .asyncRun();
+        return true;
     }
 
-    private static void _scheduleAtFixedRate(TS_ThreadSyncTrigger killTrigger, Duration until, TGS_RunnableType1<TS_ThreadSyncTrigger> exe, long initialDelay, long period, TimeUnit unit) {
+    private static boolean _scheduleAtFixedRate(TS_ThreadSyncTrigger killTrigger, Duration until, TGS_RunnableType1<TS_ThreadSyncTrigger> exe, long initialDelay, long period, TimeUnit unit) {
         Runnable exe2 = () -> {
             TS_ThreadAsyncAwait.runUntil(killTrigger, until, kt -> {
                 exe.run(kt);
             });
         };
-        _scheduleAtFixedRate(killTrigger, exe2, initialDelay, period, unit);
+        return _scheduleAtFixedRate(killTrigger, exe2, initialDelay, period, unit);
     }
 
-    public static void every(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, Duration initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
-        everySeconds(killTrigger, until, startNow, initialDelayAndPeriod.toSeconds(), exe);
+    public static boolean every(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, Duration initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
+        return everySeconds(killTrigger, until, startNow, initialDelayAndPeriod.toSeconds(), exe);
     }
 
-    public static void everySeconds(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
-        _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.SECONDS);
+    public static boolean everySeconds(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
+        return _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.SECONDS);
     }
 
-    public static void everyMinutes(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
-        _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.MINUTES);
+    public static boolean everyMinutes(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
+        return _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.MINUTES);
     }
 
-    public static void everyHours(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
-        _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.HOURS);
+    public static boolean everyHours(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
+        return _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.HOURS);
     }
 
-    public static void everyDays(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
-        _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.DAYS);
+    public static boolean everyDays(TS_ThreadSyncTrigger killTrigger, Duration until, boolean startNow, long initialDelayAndPeriod, TGS_RunnableType1<TS_ThreadSyncTrigger> exe) {
+        return _scheduleAtFixedRate(killTrigger, until, exe, startNow ? 0 : initialDelayAndPeriod, initialDelayAndPeriod, TimeUnit.DAYS);
     }
 }
