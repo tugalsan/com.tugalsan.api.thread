1a3ba589d15ad7eab4eec12448b2cc8a3440cb99
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-10 10:36:50 +0300

    .

--- src/main/java/com/tugalsan/api/thread/server/async/builder/TS_ThreadAsyncBuilder1Name.java
+++ src/main/java/com/tugalsan/api/thread/server/async/builder/TS_ThreadAsyncBuilder1Name.java
@@ -1,9 +1,11 @@
 package com.tugalsan.api.thread.server.async.builder;
 
+import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_In1;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import java.time.Duration;
 import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_In2;
 import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_OutTyped;
+import java.util.Optional;
 
 public class TS_ThreadAsyncBuilder1Name<T> {
 
@@ -39,4 +41,27 @@ public class TS_ThreadAsyncBuilder1Name<T> {
         });
     }
 
+    public TS_ThreadAsyncBuilderObject<T> asyncRun(TGS_FuncMTUCE_In1<TS_ThreadSyncTrigger> killTrigger) {
+        TGS_FuncMTUCE_In2<TS_ThreadSyncTrigger, T> killTrigger_initObj = (kt, initObj) -> killTrigger.run(kt);
+        var main = new TS_ThreadAsyncBuilder3Main(this.killTrigger, "Unnamed", TS_ThreadAsyncBuilderCallableTimed.of(), TS_ThreadAsyncBuilderRunnableTimedType2.run(killTrigger_initObj));
+        return TS_ThreadAsyncBuilderObject.of(main.killTrigger, main.name, main.init, main.main, TS_ThreadAsyncBuilderRunnableTimedType1.empty(), Optional.empty(), Optional.empty()).asyncRun();
+    }
+
+    public TS_ThreadAsyncBuilderObject<T> asyncRun(Duration max, TGS_FuncMTUCE_In1<TS_ThreadSyncTrigger> killTrigger) {
+        TGS_FuncMTUCE_In2<TS_ThreadSyncTrigger, T> killTrigger_initObj = (kt, initObj) -> killTrigger.run(kt);
+        var main = new TS_ThreadAsyncBuilder3Main(this.killTrigger, "Unnamed", TS_ThreadAsyncBuilderCallableTimed.of(), TS_ThreadAsyncBuilderRunnableTimedType2.maxTimedRun(max, killTrigger_initObj));
+        return TS_ThreadAsyncBuilderObject.of(main.killTrigger, main.name, main.init, main.main, TS_ThreadAsyncBuilderRunnableTimedType1.empty(), Optional.empty(), Optional.empty()).asyncRun();
+    }
+
+    public TS_ThreadAsyncBuilderObject<T> asyncRunAwait(TGS_FuncMTUCE_In1<TS_ThreadSyncTrigger> killTrigger) {
+        TGS_FuncMTUCE_In2<TS_ThreadSyncTrigger, T> killTrigger_initObj = (kt, initObj) -> killTrigger.run(kt);
+        var main = new TS_ThreadAsyncBuilder3Main(this.killTrigger, "Unnamed", TS_ThreadAsyncBuilderCallableTimed.of(), TS_ThreadAsyncBuilderRunnableTimedType2.run(killTrigger_initObj));
+        return TS_ThreadAsyncBuilderObject.of(main.killTrigger, main.name, main.init, main.main, TS_ThreadAsyncBuilderRunnableTimedType1.empty(), Optional.empty(), Optional.empty()).asyncRunAwait();
+    }
+
+    public TS_ThreadAsyncBuilderObject<T> asyncRunAwait(Duration max, TGS_FuncMTUCE_In1<TS_ThreadSyncTrigger> killTrigger) {
+        TGS_FuncMTUCE_In2<TS_ThreadSyncTrigger, T> killTrigger_initObj = (kt, initObj) -> killTrigger.run(kt);
+        var main = new TS_ThreadAsyncBuilder3Main(this.killTrigger, "Unnamed", TS_ThreadAsyncBuilderCallableTimed.of(), TS_ThreadAsyncBuilderRunnableTimedType2.run(killTrigger_initObj));
+        return TS_ThreadAsyncBuilderObject.of(main.killTrigger, main.name, main.init, main.main, TS_ThreadAsyncBuilderRunnableTimedType1.empty(), Optional.empty(), Optional.empty()).asyncRunAwait(max);
+    }
 }
