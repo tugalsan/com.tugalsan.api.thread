403ce156b20344a9ba2cf48e3474872807990c9e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-10-02 10:51:15 +0300

    Update TS_ThreadSyncSemaphore.java

--- src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncSemaphore.java
+++ src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncSemaphore.java
@@ -1,15 +1,17 @@
 package com.tugalsan.api.thread.server.sync;
 
+import java.util.concurrent.*;
+
 public class TS_ThreadSyncSemaphore extends Semaphore {
 
     private final int maxPermits;
 
-    public UsedTrackingSemaphore(int permits) {
+    public TS_ThreadSyncSemaphore(int permits) {
         super(permits);
         maxPermits = permits;
     }
 
-    public UsedTrackingSemaphore(int permits, boolean fair) {
+    public TS_ThreadSyncSemaphore(int permits, boolean fair) {
         super(permits, fair);
         maxPermits = permits;
     }
