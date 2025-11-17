c2db360a70dd86fa87bc7ce31ebfffa5e7f5a11e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-11-17 10:03:06 +0300

    Create TS_ThreadSyncBuffer.java

---
+++ src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncBuffer.java
@@ -0,0 +1,42 @@
+package com.tugalsan.api.thread.server.sync;
+
+import java.util.List;
+
+import module  com.tugalsan.api.function;
+
+public class TS_ThreadSyncBuffer<T> {
+
+    private TS_ThreadSyncBuffer(TS_ThreadSyncLst<T> lst, TGS_FuncMTC_OutBool_In1<T> exists) {
+        this.lst = lst;
+    }
+    final public TS_ThreadSyncLst<T> lst;
+    final public TGS_FuncMTC_OutBool_In1<T, T> exists;
+
+    public static <T> TS_ThreadSyncBuffer<T> ofSlowRead() {
+        return new TS_ThreadSyncBuffer(TS_ThreadSyncLst.ofSlowRead());
+    }
+
+    public static <T> TS_ThreadSyncBuffer<T> ofSlowWrite() {
+        return new TS_ThreadSyncBuffer(TS_ThreadSyncLst.ofSlowWrite());
+    }
+
+    public boolean isExists(T item) {
+        return exists.validate(item);
+    }
+
+    public T add(T item) {
+        return lst.add(item);
+    }
+
+    public List<T> add(List<T> items) {
+        return lst.add(items);
+    }
+
+    public boolean remove(T item) {
+        return lst.removeAll(item);
+    }
+
+    public TS_ThreadSyncLst<T> clear(T item) {
+        return lst.clear();
+    }
+}
