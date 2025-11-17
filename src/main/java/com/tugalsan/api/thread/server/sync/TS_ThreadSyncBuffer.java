ed9cf412d6baef173716fb61a45ef97746c1d144
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-11-17 10:09:02 +0300

    Update TS_ThreadSyncBuffer.java

--- src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncBuffer.java
+++ src/main/java/com/tugalsan/api/thread/server/sync/TS_ThreadSyncBuffer.java
@@ -1,28 +1,29 @@
 package com.tugalsan.api.thread.server.sync;
 
 import java.util.List;
+import java.util.Optional;
 
 import module  com.tugalsan.api.function;
 
 public class TS_ThreadSyncBuffer<T> {
 
-    private TS_ThreadSyncBuffer(TS_ThreadSyncLst<T> lst, TGS_FuncMTU_OutBool_In1<T> exists) {
+    private TS_ThreadSyncBuffer(TS_ThreadSyncLst<T> lst, TGS_FuncMTU_OutTyped_In1<Optional<T>, T> finder) {
         this.lst = lst;
-        this.exists = exists;
+        this.finder = finder;
     }
     final private TS_ThreadSyncLst<T> lst;
-    final private TGS_FuncMTU_OutBool_In1<T> exists;
+    final private TGS_FuncMTU_OutTyped_In1<Optional<T>, T> finder;
 
-    public static <T> TS_ThreadSyncBuffer<T> ofSlowRead(TGS_FuncMTU_OutBool_In1<T> exists) {
-        return new TS_ThreadSyncBuffer(TS_ThreadSyncLst.ofSlowRead(), exists);
+    public static <T> TS_ThreadSyncBuffer<T> ofSlowRead(TGS_FuncMTU_OutTyped_In1<Optional<T>, T> finder) {
+        return new TS_ThreadSyncBuffer(TS_ThreadSyncLst.ofSlowRead(), finder);
     }
 
-    public static <T> TS_ThreadSyncBuffer<T> ofSlowWrite(TGS_FuncMTU_OutBool_In1<T> exists) {
-        return new TS_ThreadSyncBuffer(TS_ThreadSyncLst.ofSlowWrite(), exists);
+    public static <T> TS_ThreadSyncBuffer<T> ofSlowWrite(TGS_FuncMTU_OutTyped_In1<Optional<T>, T> finder) {
+        return new TS_ThreadSyncBuffer(TS_ThreadSyncLst.ofSlowWrite(), finder);
     }
 
-    public boolean isExists(T item) {
-        return exists.validate(item);
+    public Optional<T> findAny(T item) {
+        return finder.call(item);
     }
 
     public T add(T item) {
